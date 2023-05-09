c439d0fbfa23b3698779f6f0ac5f93a53d5973cf
Aosmiles <arlin.smiles@gmail.com>
2023-05-09 13:13:12 -0700

    add connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 import morgan from "morgan";
 import journalEntryRoutes from "./routes/journalEntryRoutes.js";
 
@@ -15,6 +16,16 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/journal", journalEntryRoutes);
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
