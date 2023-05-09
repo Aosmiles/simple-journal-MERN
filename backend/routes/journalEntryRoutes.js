1899c8979299d2cf41b3fc683040b0118849bde8
Aosmiles <arlin.smiles@gmail.com>
2023-05-09 12:35:27 -0700

    add routes

--- backend/routes/journalEntryRoutes.js
+++ backend/routes/journalEntryRoutes.js
@@ -2,8 +2,29 @@ import express from "express";
 
 const router = express.Router();
 
+//get all entries
 router.get("/", (req, res) => {
-  res.json({ message: "hello from journal entry router" });
+  res.json({ message: "GET all entries" });
+});
+
+//get single entry
+router.get("/:id", (req, res) => {
+  res.json({ message: "GET single entry with id" });
+});
+
+//create new entry
+router.post("/", (req, res) => {
+  res.json({ message: "POST new entry" });
+});
+
+//update entry
+router.put("/:id", (req, res) => {
+  res.json({ message: `PUT update entry with id ${req.params.id}` });
+});
+
+//delete entry
+router.delete("/:id", (req, res) => {
+  res.json({ message: `DELETE entry with id ${req.params.id}` });
 });
 
 export default router;
