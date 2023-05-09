3f2cd306a6062802fdfd48da0d65495b2d693fd4
Aosmiles <arlin.smiles@gmail.com>
2023-05-09 11:27:32 -0700

    add middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import morgan from "morgan";
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
 //routes
 app.get("/", (req, res) => {
