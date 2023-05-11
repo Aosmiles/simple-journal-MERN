10b603fad3b8769fddeaf00effcf8f86a88d41e3
Aosmiles <arlin.smiles@gmail.com>
2023-05-11 10:42:40 -0700

    move connection options into connect function

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const connectonOptions = {
-  dbname: process.env.MONGO_DBNAME,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
 const connectDB = async () => {
+  const connectonOptions = {
+    dbname: process.env.MONGO_DBNAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
   try {
     await mongoose.connect(process.env.MONGO_URI, connectonOptions);
   } catch (error) {
