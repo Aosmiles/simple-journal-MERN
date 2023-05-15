4ef7174a564c3a634fe257f4b79e81862af609d6
Aosmiles <arlin.smiles@gmail.com>
2023-05-15 10:47:09 -0700

    update errorhandler

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,7 +1,15 @@
 export default (error, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = error.message;
 
   const stack = process.env.NODE_ENV === "production" ? null : error.stack;
+
+  //check for cast error and return 404
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
   console.log(error.stack);
-  res.status(statusCode).json({ message: error.message, stack: stack });
+  res.status(statusCode).json({ message: message, stack: stack });
 };
