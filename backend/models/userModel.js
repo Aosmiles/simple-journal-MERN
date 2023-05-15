f83ec4f0c56ded01c8dff751d3ae49675709751f
Aosmiles <arlin.smiles@gmail.com>
2023-05-15 14:11:24 -0700

    add match password method to model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,10 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 const User = mongoose.model("user", userSchema);
 
 export default User;
