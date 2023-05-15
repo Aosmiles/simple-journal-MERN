586ab9db15c3ecbcb240ffb9a1281c25f5fd9755
Aosmiles <arlin.smiles@gmail.com>
2023-05-15 12:26:58 -0700

    add token to cookie

--- backend/utility/generateToken.js
+++ backend/utility/generateToken.js
@@ -1,8 +1,14 @@
 import jsonwebtoken from "jsonwebtoken";
 
+const expireDays = 14;
+
 const generateToken = (res, id) => {
-  return jsonwebtoken.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "14d",
+  const token = jsonwebtoken.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: `${expireDays}d`,
+  });
+  res.cookie("token", token, {
+    httpOnly: true,
+    maxAge: expireDays * 24 * 60 * 60 * 1000,
   });
 };
 
