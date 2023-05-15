81f0572a7349b7814b150aa788d378ce0476811d
Aosmiles <arlin.smiles@gmail.com>
2023-05-15 12:09:20 -0700

    add user routes from controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,16 @@
 import express from "express";
+import userController from "../controllers/userController.js";
 
 const router = express.Router();
+
+router.post("/", userController.registerUser);
+
+router.post("/auth", userController.authUser);
+
+router.get("/logout", userController.logout);
+
+router.get("/profile", userController.getUserProfile);
+
+router.put("/profile", userController.updateUserProfile);
 
 export default router;
