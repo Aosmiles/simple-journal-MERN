b1538099bc5e66fd63d7edf2cc1aa3396805c6ac
Aosmiles <arlin.smiles@gmail.com>
2023-05-23 11:40:43 -0700

    add styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="container">
+    <header className="container m-auto p-5">
       <div className="text-4xl text-red-400 text-center">Journal App</div>
     </header>
   );
